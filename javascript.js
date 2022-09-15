b3fa27397f8600856fb6c4f2c4abfa6888dfa862
MohmmedAlherthani <mohmmedasad110@gmail.com>
2022-09-15 06:57:27 +0300

    add style

--- javascript.js
+++ javascript.js
@@ -2,3 +2,11 @@ console.log("hello");
 alert("welcome to my site")
 let userName=prompt("Please enter your name");
 document.getElementById("userName").textContent=userName;
+let button=document.getElementById("button")
+button.onclick = function(){
+    for (let index = 1; index < 100; index++) {
+        console.log(index);        
+    }
+};
+
+
